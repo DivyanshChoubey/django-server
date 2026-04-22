12f7122299ca6a0a98c2bc1ff7da663dc328d0c0
DivyanshChoubey <divyansh.choubey01@gmail.com>
2026-04-22 14:08:19 +0530

    chore: remove unnecessary column

--- service/models/teams.py
+++ service/models/teams.py
@@ -6,9 +6,8 @@ from service.models import DepartmentMaster
 class Teams(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField(max_length=256, blank=True, null=True)
-    department = models.ForeignKey(DepartmentMaster, on_delete=models.SET_NULL, null=True)
     created_by = models.ForeignKey(AdminUser, on_delete=models.SET_NULL, null=True) 
-    isactive  = models.BooleanField(default=True)
+    is_active  = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
