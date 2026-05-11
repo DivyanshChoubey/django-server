3589e40589d193b4af63db91b68cefb65b3b9f6b
DivyanshChoubey <divyansh.choubey01@gmail.com>
2026-05-11 17:36:35 +0530

    feat: make folder sepration.

--- service/views/master/department_view.py
+++ service/views/master/department_view.py
@@ -4,10 +4,23 @@ from rest_framework.views import APIView
 
 from service.constants import Constants, ResponseMessages
 from service.models import DepartmentMaster, Users
-from service.serializers import DepartmentCreateSerializer
+from service.serializers import DepartmentSerializer, DepartmentCreateSerializer, DepartmentUpdateSerializer
 
 
-class DepartmentCreateView(APIView):
+class DepartmentView(APIView):
+    def get(self, request):
+        departments = DepartmentMaster.objects.all().order_by("-id")
+        serializer = DepartmentSerializer(departments, many =True)
+
+        return Response(
+            {
+                "success": True,
+                "message": ResponseMessages.DEPARTMENT_FETCH,
+                "data": serializer.data
+            },
+            status= status.HTTP_200_OK
+        )
+    
     def post(self,request):
         serializer = DepartmentCreateSerializer(data=request.data)
 
@@ -52,4 +65,42 @@ class DepartmentCreateView(APIView):
                 }
             },
             status = status.HTTP_201_CREATED
+        )
+
+    def patch(self, request):
+        serializer = DepartmentUpdateSerializer(data=request.data)
+
+        if not serializer.is_valid():
+            return Response(
+                {
+                    "success": False,
+                    "message": ResponseMessages.INVALID_DATA,
+                    "errors": serializer.errors
+                },
+                status = status.HTTP_400_BAD_REQUEST
+            )
+
+        department_id = serializer.validated_data.get("id")
+        name = serializer.validated_data.get("name")
+
+        try:
+            department = DepartmentMaster.objects.get(id=department_id)
+        except DepartmentMaster.DoesNotExist:
+            return Response(
+                {
+                    "success": False,
+                    "message": ResponseMessages.DEPARTMENT_NOT_FOUND
+                },
+                status = status.HTTP_400_BAD_REQUEST
+            )
+
+        department.name = name
+        department.save()
+
+        return Response(
+            {
+                "success": True,
+                "message": ResponseMessages.DEPARTMENT_UPDATED
+            },
+            status = status.HTTP_200_OK
         )
